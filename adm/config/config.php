c8465646599b97197060e46dc265445e8b448a50
fs2050 <65322717+fs2050@users.noreply.github.com>
2020-10-02 07:29:43 -0300

    Update config.php

--- adm/config/config.php
+++ adm/config/config.php
@@ -6,4 +6,4 @@ $url_host = filter_input(INPUT_SERVER, 'HTTP_HOST');
 //define('pg', "https://planbwebapps.com.br/lifuca/adm");
 //define('pgsite', "http://$url_host/liga/celke");
 //define('pg', "http://menu_dominio.com.br/celke/adm");
-define('pg', "https://www.lifuca.com.br/adm");
+define('pg', "https://localhost/adm");
